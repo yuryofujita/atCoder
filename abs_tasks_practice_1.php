568b77090c9727f3e5da9bf6ddf25806339d360f
yuryofujita <dasman74@hotmail.com>
2020-06-13 08:26:29 +0900

    Create abs_tasks_practice_1.php
    以下のコード
    https://atcoder.jp/contests/abs/tasks/practice_1

---
+++ abs_tasks_practice_1.php
@@ -0,0 +1,8 @@
+<?php
+// https://atcoder.jp/contests/abs/tasks/practice_1
+fscanf(STDIN, "%d", $a);
+fscanf(STDIN, "%d %d", $b,$c);
+fscanf(STDIN, "%s", $s);
+
+echo (string)($a + $b + $c)  . " " .$s;
+?>
